7cea2e46f8d81955527783aa612183c4cbb80aae
Arthic-Cero <35507839+Arthic-Cero@users.noreply.github.com>
2018-01-25 11:15:57 -0600

    Update WebSecurityConfig.java

--- src/main/java/com/jalc/login/WebSecurityConfig.java
+++ src/main/java/com/jalc/login/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.jalc.login;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.jalc.login.service.CustomAuthenticationSuccessHandler;
 
+@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
